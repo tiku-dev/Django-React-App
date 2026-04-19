7b522e64162f02fcefc8fc989130fd0c97b3cb76
tiku-dev <tikufaith10@gmail.com>
2026-04-19 21:47:11 +0100

    first commit

---
+++ backend/api/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        notes = Note.objects.filter(author=user)
+        return notes
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:   
+            print(serializer.errors)
+        return
+
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        notes = Note.objects.filter(author=user)
+        return notes
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,64 @@
+import {useState, useEffect} from 'react'
+import api from '../api'
+import Note from '../components/Note'
+import '../styles/Home.css'
+
+
+function Home(){
+    const [notes, setNotes] = useState([])
+    const [content, setContent] = useState('')
+    const [title, setTitle] = useState('')
+
+    useEffect(() =>{
+        getNotes()
+
+    }, [])
+
+    const getNotes = () => {
+        api.get('/api/notes/').then((res) => res.data).then((data) => {setNotes(data); console.log(data)}
+        ).catch((err) => alert(err))
+    }
+    
+    const deleteNote = (id) => {
+        api.delete(`/api/notes/delete/${id}/`).then((res) =>{
+            if(res.status === 204){
+                alert('Note deleted successfully')
+            }
+            else{
+                alert('Failed to delete note')
+            }
+             getNotes()
+        }).catch((err) => alert(err))
+               
+    }
+
+    const createNote = (e) =>{
+        e.preventDefault()
+        api.post('/api/notes/', {content, title}).then((res) => {
+            if(res.status === 201){
+                alert('Note created successfully')
+            }else{
+                alert('Failed to create note')
+            }
+             getNotes()
+        }).catch((err) => alert(err))
+    } 
+    return <div>
+        <div>
+            <h2>Notes</h2>
+            {notes.map((note) => <Note note={note} onDelete={deleteNote} key={note.id}/>)}
+        </div>
+        <h2>Create Note</h2>
+        <form onSubmit={createNote}>
+            <label htmlFor='title'>Title:</label>
+            <input type='text' id='title' value={title} name='title' onChange={(e) => setTitle(e.target.value)} required/>
+
+            <label htmlFor='content'>Content:</label>
+            <textarea type='text' id='content' value={content} name='content' onChange={(e) => setContent(e.target.value)} required></textarea>
+            <br/>
+            <input type="submit" value="Submit" />
+        </form>
+    </div>
+}
+
+export default Home
